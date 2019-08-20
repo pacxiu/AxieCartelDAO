0a0ec7a9d67ad29a24b7458d13f268a846bf550d
RafalZwolak <nedvedek111@gmail.com>
2019-08-20 17:56:44 +0200

    Style Page Not Found

--- client/src/pages/NotFound/index.js
+++ client/src/pages/NotFound/index.js
@@ -8,7 +8,7 @@ import { FullHeight, Container } from 'components/Layout';
 const NotFound = () => (
   <FullHeight className={classnames(styles.container, styles.custom)}>
     <Container>
-      Page Not Found
+      <h1 className={styles.title}>Page Not Found</h1>
     </Container>
   </FullHeight>
 );
